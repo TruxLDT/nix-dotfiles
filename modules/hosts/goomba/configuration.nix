58bf2212a6786d2c09a0e5e7303b419f1c8864a2
trux <connormoya@gmail.com>
2026-04-06 12:19:54 -0500

    v1

---
+++ modules/hosts/goomba/configuration.nix
@@ -0,0 +1,146 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.myMachineConfiguration = { pkgs, lib, ... }: {
+    # import any other modules from here
+    imports = [
+      self.nixosModules.myMachineHardware
+      self.nixosModules.niri
+    ];
+    
+	# Bootloader.
+	# boot.loader.systemd-boot.enable = true;
+	boot.loader.limine.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	# Use latest kernel.
+	boot.kernelPackages = pkgs.linuxPackages_zen;
+
+	networking.hostName = "goomba"; # Define your hostname.
+
+	# Enable networking
+	networking.networkmanager.enable = true;
+
+	# Set your time zone.
+	time.timeZone = "America/Chicago";
+
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
+	};
+
+	# Enable the X11 windowing system.
+	# You can disable this if you're only using the Wayland session.
+	services.xserver.enable = true;
+	services.displayManager.sddm = {
+		enable = true;
+		# Enables experimental Wayland support
+		wayland.enable = true;
+	};
+
+
+	# Configure keymap in X11
+	services.xserver.xkb = {
+		layout = "us";
+		variant = "";
+	};
+
+	# Enable CUPS to print documents.
+	services.printing.enable = true;
+
+	# Enable sound with pipewire.
+	services.pipewire = {
+		enable = true;
+		audio.enable = true;
+		pulse.enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+	};
+	# services.pulseaudio.enable = true;
+	# services.pulseaudio.support32Bit = true; # If compatibility with 32-bit applications is desired.
+	security.rtkit.enable = true;
+
+
+	# Enable touchpad support (enabled default in most desktopManager).
+	# services.xserver.libinput.enable = true;
+
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.trux = {
+		isNormalUser = true;
+		description = "trux";
+		shell = pkgs.fish;
+		extraGroups = [ "networkmanager" "wheel" "audio" "video" "input"];
+		packages = with pkgs; [
+
+		];
+	};
+	
+	programs.fish = {
+		enable = true;
+		shellAbbrs = {
+			ll = "ls -la";
+			nrs = "nixos-rebuild switch --flake ~/myNixOS#.myMachine";
+		};
+	};
+
+	programs.niri.enable = true;
+
+	# Install firefox.
+	programs.firefox.enable = true;
+
+	# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
+
+	services.power-profiles-daemon.enable = true;
+
+	# Enable experimental settings
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+
+	environment.systemPackages = with pkgs; [
+		bat
+		brave
+		fastfetch
+		fish
+		home-manager
+		htop
+		kdePackages.kate
+		kitty
+		lm_sensors
+		neovim
+		nix-search
+		speedtest-cli
+		kdePackages.dolphin
+		libgcc
+		tmux
+		vscode
+		unzip
+		wineWow64Packages.unstable
+		xwayland
+		playerctl
+		git
+		pavucontrol
+	];
+
+	fonts.packages = with pkgs; [
+		nerd-fonts.fira-code
+		nerd-fonts.droid-sans-mono
+		font-awesome_4
+
+	];
+
+
+	# This value determines the NixOS release from which the default
+	system.stateVersion = "25.11"; # Did you read the comment?
+	};
+}
---
+++ modules/hosts/goomba/hardware.nix
@@ -0,0 +1,31 @@
+{ self, inputs, ...}: {
+  flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }: {
+
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+      ];
+
+    boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "uas" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/c503d8d0-e083-43f7-9cf6-d0d79fc103cb";
+        fsType = "ext4";
+      };
+
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/39D8-9696";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+
+    swapDevices =
+      [ { device = "/dev/disk/by-uuid/fdea71a3-139c-421f-b254-391014f633f7"; }
+      ];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+};
+}
